e0de5728ed6e5bb9cdd10145d7dbc33a2a287541
by柯乐 <1010456736@qq.com>
2024-04-02 09:51:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -64,3 +64,35 @@ let res
   $(function () {
     $('[data-toggle="popover"]').popover()
   })
+console.log("\n %c 暗号:qdqqd %c by 柯乐 | qdqqd.com ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.onclick = function(event) {
+        var heart = document.createElement("b");
+        heart.onselectstart = new Function('event.returnValue=false');
+        document.body.appendChild(heart).innerHTML = "❤";
+        heart.style.cssText = "position: fixed;left:-100%;";
+        var f = 16, // 字体大小
+            x = event.clientX - f / 2, // 横坐标
+            y = event.clientY - f, // 纵坐标
+            c = randomColor(), // 随机颜色
+            a = 1, // 透明度
+            s = 1.2; // 放大缩小
+        var timer = setInterval(function() {
+            if (a <= 0) {
+                document.body.removeChild(heart);
+                clearInterval(timer);
+            } else {
+                heart.style.cssText = "font-size:16px;cursor: default;position: fixed;color:" + c + ";left:" + x + "px;top:" + y + "px;opacity:" + a + ";transform:scale(" + s + ");";
+                y--;
+                a -= 0.016;
+                s += 0.002;
+            }
+        }, 12)
+    }
+
+    // 随机颜色
+    function randomColor() {
+        return "rgb(" + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + "," + (~~(Math.random() * 255)) + ")";
+    }
+});
